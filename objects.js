e656782800639decf18a1fb2cbc0088ca58c56b9
Mariana Sickler <marianaleal_93@hotmail.com>
2017-09-05 01:37:58 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,10 +1,11 @@
 var playlist = { Beatles: "Blackbird" };
 
 function updatePlaylist(playlist, artistName, songTitle) {
-  return Object.assign({}, playlist, [artistName]: songTitle)
+  return Object.assign({}, playlist, {[artistName]: songTitle})
 }
 
 function removeFromPlaylist(Object, artistName) {
   delete Object[artistName]
   return Object
 }
+
